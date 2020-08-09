941c9331b4cc21dd29a826375e5a290d3bfd8be6
iMAD <imad.yamani@ulb.be>
2020-08-09 22:53:06 +0200

    Update twitter-bot.py

--- twitter-bot.py
+++ twitter-bot.py
@@ -11,3 +11,7 @@ ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
 
 bot = Twython(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+"""
+bounds is for setting the area you want to acquire (latitude (north), latitude (south), longitude (west), latitude (east). If not set, all data is covered.
+"""

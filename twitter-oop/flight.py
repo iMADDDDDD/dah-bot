3445e7285cf52a74f98d1f37f5e1586a522de483
iMAD <imad.yamani@ulb.be>
2020-08-13 01:31:30 +0200

    First attempt to OOP approach

---
+++ twitter-oop/flight.py
@@ -0,0 +1,13 @@
+# Flight class
+class Flight():
+
+    # Instance attribute
+    def __init__(self, flight_id, aircraft_id, aircraft_type, aircraft_from, aircraft_to, squawk_code):
+        self.flight_id = flight_id
+        self.aircraft_id = aircraft_id
+        self.aircraft_type = aircraft_type
+        self.aircraft_from = aircraft_from
+        self.aircraft_to = aircraft_id
+        self.squawk_code = squawk_code
+
+    pass
---
+++ twitter-oop/handler.py
@@ -0,0 +1,8 @@
+import requests
+
+from flight import Flight
+
+
+# HTTP Handler
+class Handler():
+    pass
